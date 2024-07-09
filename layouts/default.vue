8c9b803f4874b77ceaa9c0500e2f0456a19f48fd
Violet Lauro <violetsnow08@gmail.com>
2024-07-09 17:29:55 -0400

    Added layouts, pages, and components folder

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <navbar />
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+<template>
+  <div>
+    <h1>Hello!</h1>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
