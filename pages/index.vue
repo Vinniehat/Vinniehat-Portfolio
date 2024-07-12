f566bb74a35fecf943b867478d536434ac0f2341
Violet Lauro <violetsnow08@gmail.com>
2024-07-11 21:08:30 -0400

    Updated index

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,21 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="grid">
-    <div>
+  <div class="flex justify-center">
+    <div class="grid grid-cols-2 gap-4" style="height: 85vh">
+      <div class="flex flex-col justify-center items-start">
+        <p class="mb-4 text-xl text-center">Hi there, I am</p>
+        <p class="mb-4 text-6xl text-center">Vincent Lauro</p>
+      </div>
 
+      <div class="flex flex-col overflow-auto justify-center items-center">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+        <Button label="Check" icon="pi pi-check"/>
+      </div>
     </div>
-    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-
-    <Button label="Check" icon="pi pi-check" />
-
   </div>
-
 </template>
 
 <style scoped>
-
+/* No additional styles needed here as Tailwind classes are used */
 </style>
